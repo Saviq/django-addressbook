4b544a4ca3399cc7c418a8681fef7eb1bc30eea5
Justin Driscoll <justin.driscoll@gmail.com>
2009-03-29 18:58:22 -0400

    Removed leftover views/urls

--- addressbook/urls.py
+++ addressbook/urls.py
@@ -2,11 +2,6 @@ from django.conf.urls.defaults import *
 from django.contrib import databrowse
 from models import *
 
-urlpatterns = patterns('addressbook.views', 
-    url(r'^$', 'main', name='addressbook-main'),
-    url(r'^contact/(?P<contact_id>\d+)/$', 'contact_detail', name='addressbook-contact-detail'),
-)
-
 # databrowse
 databrowse.site.register(Contact)
 databrowse.site.register(Group)
